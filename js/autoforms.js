0b5d96b7f0cd2c2af22c123bfc94ec8774a39259
Dmitriy.Gajewski <dmit8815@gmail.com>
2016-06-16 10:16:51 +0300

    continue reworking old code.
    - Autoform object
    - Field object
    - Field validate method (start of rework)

--- js/autoforms.js
+++ js/autoforms.js
@@ -38,21 +38,97 @@
 
 "use strict";
 
+
+
+var AutoForm = function (htmlElementNode, options) {
+    var thisAutoform = this;
+
+    var Field = function (node) {
+        this.node = node;
+        this.data = node.dataset;
+        this.empty = false;
+        this.valid = false;
+    };
+
+    Field.prototype.isValid = function () {
+        this.empty = this.node.value === "";
+        if (!this.empty ) { // если поле не пустое
+            switch (this.data.fieldType) { // начинаем валидацию
+                case "text-all": this.valid = true; break; // любой текст
+                case "text-url": this.valid = true; break; // аналогично верхнему
+                case "date": this.valid = true; break;
+                case "phone": this.valid = true; break;
+                case "maskphone": this.valid = this.node.value.indexOf("_")<0; break;
+                case "radio": this.valid = thisAutoform.querySelector("input[name='"+this.node.getAttribute("name")+"']:checked").value != undefined||(!this.data.required); break;
+                case "email":  // в e-mail проверяем наличие символов @ и точки
+                    this.valid = ((String(this.node.value).indexOf("@") > -1) && (String(this.node.value).indexOf(".") > -1));
+                    if (!this.valid) { thisAutoform.errorString = "Неправильный email"; }
+                    break;
+                case "checkbox":
+                    this.valid = true; break;
+                case "number": this.valid = true; break;
+            }
+            if (this.data.crossValid) {
+                if (document.querySelector("#"+this.data.crossValid).value !== "") this.valid = true;
+            }
+            // после валидации возвращаем значение valid. Соответственно если валидация не прошла
+            // или тип валидации не установлен вернется valid = false
+            return this.valid;
+        }
+        else {
+            // если поле пустое
+            if ((this.data.required !== true)&&(this.data.required !== undefined)) {
+                // но не обязательное
+                this.valid = true;
+                // пропускаем его
+                return this.valid;
+            }
+            else {
+                //   console.log(this);
+                // но если всё же обязательное
+                thisAutoform.errorString = "Незаполнены обязательные поля";
+                // отмечаем как не валидное и устанавливаем строку ошибки
+                this.valid = false;
+                if (this.data.crossValid) {
+                    if (document.querySelector("#"+this.data.crossValid).value !== "") this.valid = true;
+                }
+                return this.valid;
+            }
+        }
+    };
+    this.options = {
+        ShowErrorMsg       : options.ShowErrorMsg||false,
+        ErrorMsgContainer  : options.ErrorMsgContainer||".autoforms-errors",
+        EnableAnimations   : options.EnableAnimations||true,
+        DeactivateSubmit   : options.DeactivateSubmit||true,
+        FormInvalidClass   : options.FormInvalidClass||true,
+        InvalidKeyErrorMsg : options.InvalidKeyErrorMsg||true,
+        InvalidKeyTimeout  : options.InvalidKeyTimeout||1000,
+        CancelButton       : options.CancelButton||".cancel",
+        CancelErrorMsg     : options.CancelErrorMsg||false,
+        PositiveValidation : options.PositiveValidation||true
+    };
+    this.node = htmlElementNode;
+    this.submit = this.node.querySelector('input[type="submit"]').length < 1?document.querySelector('input[form="' + this.node.id + '"]'):this.node.querySelector('input[type="submit"]');
+    this.fields = [];
+    var fields = this.node.querySelectorAll('input[type="text"], input[type="password"], input[type="checkbox"], input[type="radio"], select, textarea input[type="text"][form="' + this.node.id + '"], select[form="' + this.node.id + '"], input[type="radio"][form="' + this.node.id + '"]');
+    for (var i=0; i< fields.length; i++) {
+        this.fields.push(new Field(fields[i]));
+    }
+    this.errorString = "";
+};
+
+
 window.autoform = {
     widgets: {}, // all widgets with inited autoform
     init: function (htmlElementNode, options) {
-        var opts = {
-            ShowErrorMsg       : options.ShowErrorMsg||false,
-            ErrorMsgContainer  : options.ErrorMsgContainer||".autoforms-errors",
-            EnableAnimations   : options.EnableAnimations||true,
-            DeactivateSubmit   : options.DeactivateSubmit||true,
-            FormInvalidClass   : options.FormInvalidClass||true,
-            InvalidKeyErrorMsg : options.InvalidKeyErrorMsg||true,
-            InvalidKeyTimeout  : options.InvalidKeyTimeout||1000,
-            CancelButton       : options.CancelButton||".cancel",
-            CancelErrorMsg     : options.CancelErrorMsg||false,
-            PositiveValidation : options.PositiveValidation||true
-        };
-        console.log(htmlElementNode);
+        var aufm = this,
+
+            newElementName = (htmlElementNode.className+htmlElementNode.id).toLowerCase().replace(new RegExp("[^[a-zA-Z0-9]]*","g"),'_');
+
+        if (!options) options = {};
+
+        var newAufmWidget = aufm.widgets[newElementName] = new AutoForm(htmlElementNode, options);
+
     }
 };
