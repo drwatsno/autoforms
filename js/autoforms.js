434421b2cffb19540c34fe7e9a2ae2be65487be9
Dmitriy.Gajewski <dmit8815@gmail.com>
2016-06-17 10:59:00 +0300

    continue reworking old code.
    - init Events
    - renamed functions
    - translated comments
    - form validate Event

--- js/autoforms.js
+++ js/autoforms.js
@@ -18,8 +18,8 @@
 /* 1.00
 
  Plugin is designed to automatically validate the form when you hover on submit associated with this form
- For proper operation of the script fields of the form you need to add the type of validation that is transmitted through the attribute
- data-field-type. Optional fields are marked attribute data-required = 'false'.
+ For proper operation of the script, you need to add the type of validation attribute 'data-field-type' to fields.
+ Optional fields are marked with attribute data-required = 'false'.
 
  *
  *  Usage: autoform.init(htmlObject,options)
@@ -38,8 +38,6 @@
 
 "use strict";
 
-
-
 var AutoForm = function (htmlElementNode, options) {
     var thisAutoform = this;
 
@@ -50,44 +48,43 @@ var AutoForm = function (htmlElementNode, options) {
         this.valid = false;
     };
 
-    Field.prototype.isValid = function () {
+    Field.prototype.validate = function () {
         this.empty = this.node.value === "";
-        if (!this.empty ) { // если поле не пустое
-            switch (this.data.fieldType) { // начинаем валидацию
-                case "text-all": this.valid = true; break; // любой текст
-                case "text-url": this.valid = true; break; // аналогично верхнему
-                case "date": this.valid = true; break;
-                case "phone": this.valid = true; break;
+        if (!this.empty ) { // if field is not empty
+            switch (this.data.fieldType) { // begin validation
+                case "text-all": this.valid = true; break; // any text
+                case "text-url": this.valid = true; break; // any text
+                case "date": this.valid = true; break;     // TODO: validate date
+                case "phone": this.valid = true; break;    // TODO: validate phone
                 case "maskphone": this.valid = this.node.value.indexOf("_")<0; break;
                 case "radio": this.valid = thisAutoform.querySelector("input[name='"+this.node.getAttribute("name")+"']:checked").value != undefined||(!this.data.required); break;
-                case "email":  // в e-mail проверяем наличие символов @ и точки
+                case "email":  // in e-mail we search for '@' and '.' symbols
                     this.valid = ((String(this.node.value).indexOf("@") > -1) && (String(this.node.value).indexOf(".") > -1));
                     if (!this.valid) { thisAutoform.errorString = "Неправильный email"; }
                     break;
                 case "checkbox":
                     this.valid = true; break;
-                case "number": this.valid = true; break;
+                case "number": this.valid = true; break; // TODO: validate numbers
             }
             if (this.data.crossValid) {
                 if (document.querySelector("#"+this.data.crossValid).value !== "") this.valid = true;
             }
-            // после валидации возвращаем значение valid. Соответственно если валидация не прошла
-            // или тип валидации не установлен вернется valid = false
+            // returning this.valid property after validation
             return this.valid;
         }
         else {
-            // если поле пустое
+            // field is empty
             if ((this.data.required !== true)&&(this.data.required !== undefined)) {
-                // но не обязательное
+                // but not required
                 this.valid = true;
-                // пропускаем его
+                // skipping
                 return this.valid;
             }
             else {
                 //   console.log(this);
-                // но если всё же обязательное
+                //  but if required
                 thisAutoform.errorString = "Незаполнены обязательные поля";
-                // отмечаем как не валидное и устанавливаем строку ошибки
+                // marking as not valid and changing errorString
                 this.valid = false;
                 if (this.data.crossValid) {
                     if (document.querySelector("#"+this.data.crossValid).value !== "") this.valid = true;
@@ -108,16 +105,152 @@ var AutoForm = function (htmlElementNode, options) {
         CancelErrorMsg     : options.CancelErrorMsg||false,
         PositiveValidation : options.PositiveValidation||true
     };
+    this.valid = false;
     this.node = htmlElementNode;
+    this.errorString = "";
     this.submit = this.node.querySelector('input[type="submit"]').length < 1?document.querySelector('input[form="' + this.node.id + '"]'):this.node.querySelector('input[type="submit"]');
     this.fields = [];
     var fields = this.node.querySelectorAll('input[type="text"], input[type="password"], input[type="checkbox"], input[type="radio"], select, textarea input[type="text"][form="' + this.node.id + '"], select[form="' + this.node.id + '"], input[type="radio"][form="' + this.node.id + '"]');
     for (var i=0; i< fields.length; i++) {
         this.fields.push(new Field(fields[i]));
     }
-    this.errorString = "";
 };
 
+AutoForm.prototype.validate = function () {
+    this.valid = true;
+    for (var i=0; i< this.fields.length; i++) {
+
+        if (this.fields[i].validate()) {
+        }
+        else {
+            this.valid = false;
+        }
+    }
+    return this.valid;
+};
+
+AutoForm.prototype.initEvents = function () {
+    var _this = this;
+
+    function validateCheckEventsRun() {
+        if (_this.validate()) {
+            if (_this.options.FormInvalidClass) {
+                _this.node.classList.remove("autoform-form-invalid");
+            }
+            if (_this.options.DeactivateSubmit) {
+                _this.submit.parentElement.classList.remove("autoform-submit-invalid");
+                _this.submit.attributes.removeNamedItem("disabled");
+            }
+        }
+        else {
+            if (_this.options.FormInvalidClass) {
+                _this.node.classList.add("autoform-form-invalid");
+            }
+            if (_this.options.DeactivateSubmit) {
+                _this.submit.parentElement.classList.add("autoform-submit-invalid");
+                _this.submit.attributes.disabled = "disabled";
+            }
+        }
+    }
+
+    for (var i=0; i< _this.fields.length; i++) {
+        (function () {
+            var currentField = _this.fields[i];
+            var noLimit = false,
+                checkString,
+                keyErrWrap,
+                additionalValidation = true;
+
+            currentField.node.addEventListener("keyup", function () {
+                validateCheckEventsRun();
+            });
+            currentField.node.addEventListener("change", function () {
+                validateCheckEventsRun();
+            });
+            currentField.node.addEventListener("click", function () {
+                validateCheckEventsRun();
+                document.querySelector(_this.options.ErrorMsgContainer).innerHTML = "";
+                this.classList.remove("autoform-invalid");
+            });
+            currentField.node.addEventListener("keypress", function (evt) {
+                var invalidKeyErrorMsg = "Недопустимый символ";
+                if (evt.keyCode === 13) {
+                    if ((_this.submit.attributes.disabled !== 'disabled')&&(this.tagName!=="TEXTAREA")) {
+                        _this.submit.click();
+                    }
+                }
+
+                switch (currentField.data.fieldType) {
+                    case "text-all": noLimit = true; break;
+                    case "checkbox": noLimit = true; break;
+                    case "text-url": checkString = "13 49 50 51 52 53 54 55 56 57 48 45 61 95 43 113 119 101 114 116 121 117 105 111 112 91 93 97 115 100 102 103 104 106 107 108 59 39 122 120 99 118 98 110 109 44 46 47 81 87 69 82 84 89 85 73 79 80 123 125 124 65 83 68 70 71 72 74 75 76 58 90 88 67 86 66 78 77 60 62 63";
+                        invalidKeyErrorMsg = "Используйте только латинницу";
+                        break;
+                    case "date":     checkString = "13 47 46 49 50 51 52 53 54 55 56 57 48";
+                        additionalValidation = (currentField.node.value.length < 10);
+                        invalidKeyErrorMsg = "Используйте только цифры и разделители";
+                        break;
+                    case "email":   checkString = "13 48 49 50 51 52 53 54 55 56 57 46 64 113 119 101 114 116 121 117 105 111 112 97 115 100 102 103 104 106 107 108 122 120 99 118 98 110 109 45 81 87 69 82 84 89 85 73 79 80 65 83 68 70 71 72 74 75 76 90 88 67 86 66 78 77";
+                        invalidKeyErrorMsg = "Используйте только латинницу";
+                        break;
+                    case "phone":   checkString = "40 41 43 45 13 48 49 50 51 52 53 54 55 56 57 40 41 45";
+                        invalidKeyErrorMsg = "Используйте только цифры";
+                        break;
+                    case "number":   checkString = "48 49 50 51 52 53 54 55 56 57";
+                        invalidKeyErrorMsg = "Используйте только цифры";
+                        break;
+                    case "maskphone":   checkString = "40 41 43 45 13 48 49 50 51 52 53 54 55 56 57 40 41 45";
+                        invalidKeyErrorMsg = "Используйте только цифры";
+                        break;
+                    case "hunter-certificate":
+                        noLimit = true;
+                        break;
+
+                }
+                if (additionalValidation && (!noLimit) && (checkString.search(evt.which) === -1)) {
+                    if (_this.options.InvalidKeyErrorMsg) {
+                        if (currentField.data.keyerrwrapid) {
+                            keyErrWrap = document.querySelector("#" + currentField.data.keyerrwrapid);
+                        } else {
+                            keyErrWrap = document.querySelector("#autoforms_errors");
+                            if (keyErrWrap.length < 1) {
+                                document.querySelector(_this.options.ErrorMsgContainer).innerHTML = '<div id="autoforms_errors" style="opacity: 0"></div>';
+                                keyErrWrap = document.querySelector("#autoforms_errors");
+                            }
+                        }
+
+                        keyErrWrap.style.opacity = 1;
+                        if (keyErrWrap.querySelector(".keyerr").length < 1) {
+                            keyErrWrap.innerHTML = keyErrWrap.innerHTML + '<span class="keyerr" style="opacity: 1">' + invalidKeyErrorMsg + '</span>';
+                            setTimeout(function () {
+                                keyErrWrap.querySelector(".keyerr").style.opacity = 0;
+                                keyErrWrap.querySelector(".keyerr").remove();
+                            }, 900);
+                        }
+
+                    }
+                    return false;
+                } else {
+                    if (_this.options.InvalidKeyErrorMsg) {
+                        document.querySelector("#autoforms_errors .keyerr").style.opacity = 0;
+                        document.querySelector("#autoforms_errors .keyerr").remove();
+                    }
+                }
+            });
+
+            if (_this.options.PositiveValidation) {
+                currentField.node.addEventListener("focusout", function(){
+                    if (currentField.validate()) {
+                        currentField.classList.add("valid");
+                    }
+                });
+                currentField.node.addEventListener("focusin", function(){
+                    currentField.classList.remove("valid");
+                });
+            }
+        })();
+    }
+};
 
 window.autoform = {
     widgets: {}, // all widgets with inited autoform
@@ -129,6 +262,7 @@ window.autoform = {
         if (!options) options = {};
 
         var newAufmWidget = aufm.widgets[newElementName] = new AutoForm(htmlElementNode, options);
+            newAufmWidget.initEvents();
 
     }
 };
