84cbf7f2140da50878696333ae638ec55da22ae1
Dmitriy.Gajewski <dmit8815@gmail.com>
2016-08-18 10:44:36 +0300

    dummy validator for "select" field type fix for leave highlights option (field now remain highlighted on focusout  if its not valid)

--- js/es6/autoforms.js
+++ js/es6/autoforms.js
@@ -212,6 +212,12 @@ class AutoForm {
                     },
                     "keypressValidatorFunction":false
                 },
+                "select": {
+                    "keys": "",
+                    "errorMessage": "",
+                    "validatorFunction": false,
+                    "keypressValidatorFunction": false
+                },
                 "email": {
                     "keys":"13 48 49 50 51 52 53 54 55 56 57 46 64 113 119 101 114 116 121 117 105 111 112 97 115 100 102 103 104 106 107 108 122 120 99 118 98 110 109 45 81 87 69 82 84 89 85 73 79 80 65 83 68 70 71 72 74 75 76 90 88 67 86 66 78 77",
                     "errorMessage":"Type only latin",
@@ -407,6 +413,9 @@ class AutoForm {
 
         _this.submit.parentNode.addEventListener("mouseenter", function () {
             _this.highlightInvalidFields("on");
+            if (!_this._node.classList.contains(AUTOFORM_HOVERED_ONCE)) {
+                _this._node.classList.add(AUTOFORM_HOVERED_ONCE);
+            }
             if (_this.valid) {
             }
             else {
