e3af0543de2c8b167cd25d9ea7c9e615ce53bd51
Dmitriy Gajewski <dmit8815@gmail.com>
2016-08-23 00:12:31 +0300

    - readable keys validation string - query all html5 inputs - query button submit - use 'type' attribute of field as fallback method to determine which validator fits to it

--- js/compiled/autoforms.js
+++ js/compiled/autoforms.js
@@ -29,6 +29,7 @@ var AUTOFORM_SUBMIT_INVALID_CLASS = "autoform-submit-invalid";
 var AUTOFORM_KEYERROR_CLASS = "keyerr";
 var AUTOFORM_HOVERED_ONCE = "autoform-submit-hovered-once";
 var AUTOFORM_KEYERROR_WRAP_CLASS = "autoforms_errors";
+var HTML5_INPUT_TYPES = ['text', 'password', 'checkbox', 'radio', 'number', 'color', 'date', 'datetime', 'datetime-local', 'email', 'range', 'search', 'tel', 'time', 'url', 'month', 'week'];
 
 var Field = function () {
     /**
@@ -44,6 +45,7 @@ var Field = function () {
         currentField._node = node;
         node.autoformField = currentField;
         currentField._data = node.dataset;
+        currentField.type = currentField._data.fieldType || currentField._node.attributes.type.value;
         currentField.empty = false;
         currentField.valid = false;
         currentField._autoForm = autoForm;
@@ -84,11 +86,13 @@ var Field = function () {
                     currentField._autoForm.submit.click();
                 }
 
-                if (currentField._autoForm.options.Validators[currentField._data.fieldType].keypressValidatorFunction) {
-                    additionalValidation = currentField._autoForm.options.Validators[currentField._data.fieldType].keypressValidatorFunction(currentField);
+                if (currentField._autoForm.options.Validators[currentField.type].keypressValidatorFunction) {
+                    additionalValidation = currentField._autoForm.options.Validators[currentField.type].keypressValidatorFunction(currentField);
                 }
-                if (currentField._autoForm.options.Validators[currentField._data.fieldType].keys) {
-                    checkString = currentField._autoForm.options.Validators[currentField._data.fieldType].keys;
+                if (currentField._autoForm.options.Validators[currentField.type].keys) {
+                    checkString = currentField._autoForm.options.Validators[currentField.type].keys.split('').map(function (char) {
+                        return char.charCodeAt();
+                    }).join(' ') + ' 8 9 10 13';
                 } else {
                     allowAllSymbols = true;
                 }
@@ -157,9 +161,9 @@ var Field = function () {
             _this.empty = _this._node.value === "";
             if (!_this.empty) {
                 // if field is not empty
-                if (_this._autoForm.options.Validators[_this._data.fieldType]) {
-                    if (_this._autoForm.options.Validators[_this._data.fieldType].validatorFunction) {
-                        _this.valid = _this._autoForm.options.Validators[_this._data.fieldType].validatorFunction(_this);
+                if (_this._autoForm.options.Validators[_this.type]) {
+                    if (_this._autoForm.options.Validators[_this.type].validatorFunction) {
+                        _this.valid = _this._autoForm.options.Validators[_this.type].validatorFunction(_this);
                     } else {
                         _this.valid = true;
                     }
@@ -167,7 +171,7 @@ var Field = function () {
                     _this.valid = true;
                 }
             } else {
-                if (!_this._data.required && _this._data.required !== undefined) {
+                if (_this._data.required != true && _this._data.required !== undefined) {
                     _this.valid = true;
                 } else {
                     _this._autoForm.errorString = "Fill up required fields";
@@ -206,13 +210,13 @@ var AutoForm = function () {
                     "keypressValidatorFunction": false
                 },
                 "text-url": {
-                    "keys": "13 49 50 51 52 53 54 55 56 57 48 45 61 95 43 113 119 101 114 116 121 117 105 111 112 91 93 97 115 100 102 103 104 106 107 108 59 39 122 120 99 118 98 110 109 44 46 47 81 87 69 82 84 89 85 73 79 80 123 125 124 65 83 68 70 71 72 74 75 76 58 90 88 67 86 66 78 77 60 62 63",
+                    "keys": "1234567890-=_+qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?",
                     "errorMessage": "Type only latin",
                     "validatorFunction": false,
                     "keypressValidatorFunction": false
                 },
                 "date": {
-                    "keys": "13 47 46 49 50 51 52 53 54 55 56 57 48",
+                    "keys": "/.1234567890",
                     "errorMessage": "Type only numbers and delimiters",
                     "validatorFunction": false,
                     "keypressValidatorFunction": function keypressValidatorFunction(field) {
@@ -220,7 +224,7 @@ var AutoForm = function () {
                     }
                 },
                 "phone": {
-                    "keys": "40 41 43 45 13 48 49 50 51 52 53 54 55 56 57 40 41 45",
+                    "keys": "()+-0123456789()-",
                     "errorMessage": "Type only numbers",
                     "validatorFunction": false,
                     "keypressValidatorFunction": false
@@ -241,8 +245,7 @@ var AutoForm = function () {
                     "keypressValidatorFunction": false
                 },
                 "email": {
-                    "keys": "13 48 49 50 51 52 53 54 55 56 57 46 64 113 119 101 114 116 121 117 105 111 112 97 115 100 102 103 104 106 107 108 122 120 99 118 98 110 109 45 81 87 69 82 84 89 85 73 79 80 65 83 68 70 71 72 74 75 76 90 88 67 86 66 78 77",
-                    "errorMessage": "Type only latin",
+                    "keys": "0123456789.@qwertyuiopasdfghjklzxcvbnm-QWERTYUIOPASDFGHJKLZXCVBNM",
                     "validatorFunction": function validatorFunction(field) {
                         var valid = false;
                         valid = String(field._node.value).indexOf("@") > -1 && String(field._node.value).indexOf(".") > -1;
@@ -270,7 +273,7 @@ var AutoForm = function () {
                     "keypressValidatorFunction": false
                 },
                 "number": {
-                    "keys": "48 49 50 51 52 53 54 55 56 57",
+                    "keys": "0123456789",
                     "errorMessage": "Type only numbers",
                     "validatorFunction": false,
                     "keypressValidatorFunction": false
@@ -330,9 +333,12 @@ var AutoForm = function () {
         value: function updateFields() {
             var thisAutoForm = this;
 
-            this.submit = this._node.querySelector('input[type="submit"]').length < 1 ? document.querySelector('input[form="' + this._node.id + '"]') : this._node.querySelector('input[type="submit"]');
+            this.submit = this._node.querySelector('input[type="submit"]') || this._node.querySelector('button[type="submit"]') || document.querySelector('input[form="' + this._node.id + '"]') || document.querySelector('button[form="' + this._node.id + '"]') || this._node.querySelector('button');
             this.fields = [];
-            var fields = this._node.querySelectorAll('input[type="text"], input[type="password"], input[type="checkbox"], input[type="radio"], select, textarea, input[type="text"][form="' + this._node.id + '"], select[form="' + this._node.id + '"], input[type="radio"][form="' + this._node.id + '"]');
+            var thisNodeId = this._node.id;
+            var fields = this._node.querySelectorAll(HTML5_INPUT_TYPES.map(function (fieldTypeHTML) {
+                return "input[type=\"" + fieldTypeHTML + "\"], input[type=\"" + fieldTypeHTML + "\"][form=\"" + thisNodeId + "\"]";
+            }).join(', ') + ', select, ' + 'textarea, ' + 'select[form="' + this._node.id + '"]');
 
             var _iteratorNormalCompletion = true;
             var _didIteratorError = false;
--- js/es6/autoforms.js
+++ js/es6/autoforms.js
@@ -23,6 +23,7 @@ const AUTOFORM_SUBMIT_INVALID_CLASS = "autoform-submit-invalid";
 const AUTOFORM_KEYERROR_CLASS = "keyerr";
 const AUTOFORM_HOVERED_ONCE = "autoform-submit-hovered-once";
 const AUTOFORM_KEYERROR_WRAP_CLASS = "autoforms_errors";
+const HTML5_INPUT_TYPES = ['text','password','checkbox','radio','number','color','date','datetime','datetime-local','email','range','search','tel','time','url','month','week'];
 
 class Field {
     /**
@@ -36,6 +37,7 @@ class Field {
         currentField._node = node;
         node.autoformField = currentField;
         currentField._data = node.dataset;
+        currentField.type = currentField._data.fieldType || currentField._node.attributes.type.value;
         currentField.empty = false;
         currentField.valid = false;
         currentField._autoForm = autoForm;
@@ -68,11 +70,11 @@ class Field {
                 currentField._autoForm.submit.click();
             }
 
-            if (currentField._autoForm.options.Validators[currentField._data.fieldType].keypressValidatorFunction) {
-                additionalValidation = currentField._autoForm.options.Validators[currentField._data.fieldType].keypressValidatorFunction(currentField)
+            if (currentField._autoForm.options.Validators[currentField.type].keypressValidatorFunction) {
+                additionalValidation = currentField._autoForm.options.Validators[currentField.type].keypressValidatorFunction(currentField)
             }
-            if (currentField._autoForm.options.Validators[currentField._data.fieldType].keys) {
-                checkString = currentField._autoForm.options.Validators[currentField._data.fieldType].keys
+            if (currentField._autoForm.options.Validators[currentField.type].keys) {
+                checkString = currentField._autoForm.options.Validators[currentField.type].keys.split('').map(function(char){ return char.charCodeAt() }).join(' ')+' 8 9 10 13'
             } else {
                 allowAllSymbols = true;
             }
@@ -138,9 +140,9 @@ class Field {
         var _this = this;
         _this.empty = _this._node.value === "";
         if (!_this.empty ) { // if field is not empty
-            if (_this._autoForm.options.Validators[_this._data.fieldType]) {
-                if (_this._autoForm.options.Validators[_this._data.fieldType].validatorFunction) {
-                    _this.valid = _this._autoForm.options.Validators[_this._data.fieldType].validatorFunction(_this);
+            if (_this._autoForm.options.Validators[_this.type]) {
+                if (_this._autoForm.options.Validators[_this.type].validatorFunction) {
+                    _this.valid = _this._autoForm.options.Validators[_this.type].validatorFunction(_this);
                 } else {
                     _this.valid = true;
                 }
@@ -149,7 +151,7 @@ class Field {
             }
         }
         else {
-            if ((!_this._data.required)&&(_this._data.required !== undefined)) {
+            if ((_this._data.required != true)&&(_this._data.required !== undefined)) {
                 _this.valid = true;
             }
             else {
@@ -184,13 +186,13 @@ class AutoForm {
                     "keypressValidatorFunction":false
                 },
                 "text-url": {
-                    "keys":"13 49 50 51 52 53 54 55 56 57 48 45 61 95 43 113 119 101 114 116 121 117 105 111 112 91 93 97 115 100 102 103 104 106 107 108 59 39 122 120 99 118 98 110 109 44 46 47 81 87 69 82 84 89 85 73 79 80 123 125 124 65 83 68 70 71 72 74 75 76 58 90 88 67 86 66 78 77 60 62 63",
+                    "keys":"1234567890-=_+qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?",
                     "errorMessage":"Type only latin",
                     "validatorFunction":false,
                     "keypressValidatorFunction":false
                 },
                 "date": {
-                    "keys":"13 47 46 49 50 51 52 53 54 55 56 57 48",
+                    "keys":"/.1234567890",
                     "errorMessage":"Type only numbers and delimiters",
                     "validatorFunction":false,
                     "keypressValidatorFunction": function (field) {
@@ -198,7 +200,7 @@ class AutoForm {
                     }
                 },
                 "phone": {
-                    "keys":"40 41 43 45 13 48 49 50 51 52 53 54 55 56 57 40 41 45",
+                    "keys":"()+-0123456789()-",
                     "errorMessage":"Type only numbers",
                     "validatorFunction":false,
                     "keypressValidatorFunction":false
@@ -219,8 +221,7 @@ class AutoForm {
                     "keypressValidatorFunction": false
                 },
                 "email": {
-                    "keys":"13 48 49 50 51 52 53 54 55 56 57 46 64 113 119 101 114 116 121 117 105 111 112 97 115 100 102 103 104 106 107 108 122 120 99 118 98 110 109 45 81 87 69 82 84 89 85 73 79 80 65 83 68 70 71 72 74 75 76 90 88 67 86 66 78 77",
-                    "errorMessage":"Type only latin",
+                    "keys":"0123456789.@qwertyuiopasdfghjklzxcvbnm-QWERTYUIOPASDFGHJKLZXCVBNM",
                     "validatorFunction":function (field) {
                         let valid = false;
                         valid = ((String(field._node.value).indexOf("@") > -1) && (String(field._node.value).indexOf(".") > -1));
@@ -246,7 +247,7 @@ class AutoForm {
                     "keypressValidatorFunction":false
                 },
                 "number": {
-                    "keys":"48 49 50 51 52 53 54 55 56 57",
+                    "keys":"0123456789",
                     "errorMessage":"Type only numbers",
                     "validatorFunction":false,
                     "keypressValidatorFunction":false
@@ -302,9 +303,20 @@ class AutoForm {
     updateFields() {
         var thisAutoForm = this;
 
-        this.submit = this._node.querySelector('input[type="submit"]').length < 1?document.querySelector('input[form="' + this._node.id + '"]'):this._node.querySelector('input[type="submit"]');
+        this.submit = this._node.querySelector('input[type="submit"]') ||
+                      this._node.querySelector('button[type="submit"]') ||
+                      document.querySelector('input[form="' + this._node.id + '"]') ||
+                      document.querySelector('button[form="' + this._node.id + '"]') ||
+                      this._node.querySelector('button');
         this.fields = [];
-        let fields = this._node.querySelectorAll('input[type="text"], input[type="password"], input[type="checkbox"], input[type="radio"], select, textarea, input[type="text"][form="' + this._node.id + '"], select[form="' + this._node.id + '"], input[type="radio"][form="' + this._node.id + '"]');
+        let thisNodeId = this._node.id;
+        let fields = this._node.querySelectorAll(
+            (HTML5_INPUT_TYPES.map(function (fieldTypeHTML) {
+                return `input[type="${fieldTypeHTML}"], input[type="${fieldTypeHTML}"][form="${thisNodeId}"]`
+            }).join(', '))+
+            ', select, ' +
+            'textarea, ' +
+            'select[form="' + this._node.id + '"]');
 
         for (let field of fields) {
             this.fields.push(new Field(field,thisAutoForm));
